017fb5c602d107fec812b5a957772a2eddadd3dc
Duarte0 <guiduart3223@gmail.com>
2025-11-27 12:56:03 -0300

    refactor: limpa serviços obsoletos
    - Remove agendamento_manager.py
    - Remove cliente_status_manager.py
    - Remove webhook_handler.py
    - Mantém apenas serviços essenciais (digisac, templates, feriados)
    - Atualiza imports em __init__.py

---
+++ src/services/__init__.py
@@ -0,0 +1,11 @@
+from .digisac_service import DigisacAPI
+from .feriados_manager import FeriadosManager
+from .template_engine import TemplateEngine
+from .template_manager import TemplateManager
+
+__all__ = [
+    'DigisacAPI',
+    'FeriadosManager',
+    'TemplateEngine',
+    'TemplateManager'
+]
