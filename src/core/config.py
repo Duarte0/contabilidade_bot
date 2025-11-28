1f91f1574e771447426047b0d60e081c6736e3cb
Duarte0 <guiduart3223@gmail.com>
2025-11-28 16:39:56 -0300

    chore: ajustar config

--- src/core/config.py
+++ src/core/config.py
@@ -5,9 +5,9 @@ load_dotenv()
 
 DIGISAC_TOKEN = os.getenv('DIGISAC_API_TOKEN')
 DATABASE_PATH = 'contas.db'
-API_BASE_URL = 'https://inov.digisac.chat/api/v1'
-WEBHOOK_SECRET = os.getenv('DIGISAC_WEBHOOK_SECRET', '')
-WEBHOOK_URL = os.getenv('DIGISAC_WEBHOOK_URL', '')
+API_BASE_URL = os.getenv('DIGISAC_API_URL')
+WEBHOOK_SECRET = os.getenv('DIGISAC_WEBHOOK_SECRET')
+WEBHOOK_URL = os.getenv('DIGISAC_WEBHOOK_URL')
 
 POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'localhost')
 POSTGRES_PORT = os.getenv('POSTGRES_PORT', '5432')
